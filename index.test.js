5a814ef7c3953f88f3340dd709fc5cd0b932d4e9
Koushikm8926 <Koushik.12019976@gmail.com>
2025-02-03 22:49:32 +0100

    multiple test cases methode implemented

--- index.test.js
+++ index.test.js
@@ -2,6 +2,19 @@ const multiply = require('./index')
 
 //TEST SUITE
 
-test ('2 and 5 multiply is equal to 10',  () => {
-    expect (multiply(2,5)).toBe(10);
-});
+
+//methode 1 
+// test ('2 and 5 multiply is equal to 10',  () => {
+//     expect (multiply(2,5)).toBe(10);
+// });
+
+//methode 2
+describe ("Multiply test", ()=>{
+    it ("2 and 5 multiply would be 10", ()=>{
+        expect(multiply(2,5)).toBe(10);
+    });
+
+    it("2 and 5 multiply is not equal to 5", ()=>{
+        expect(multiply(2,5)).not.toBe(5);
+    })
+})
