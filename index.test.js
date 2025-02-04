c6cf35eff2d8b665130fd0fa65fa184805a6aa7f
Koushikm8926 <Koushik.12019976@gmail.com>
2025-02-04 11:12:28 +0100

    object equality example tested

--- index.test.js
+++ index.test.js
@@ -9,12 +9,19 @@ const multiply = require('./index')
 // });
 
 //methode 2
-describe ("Multiply test", ()=>{
-    it ("2 and 5 multiply would be 10", ()=>{
-        expect(multiply(2,5)).toBe(10);
-    });
+// describe ("Multiply test", ()=>{
+//     it ("2 and 5 multiply would be 10", ()=>{
+//         expect(multiply(2,5)).toBe(10);
+//     });
 
-    it("2 and 5 multiply is not equal to 5", ()=>{
-        expect(multiply(2,5)).not.toBe(5);
-    })
+//     it("2 and 5 multiply is not equal to 5", ()=>{
+//         expect(multiply(2,5)).not.toBe(5);
+//     })
+// })
+
+
+test ('object equality',()=>{
+    const data = {alpha :"koushik"};
+    data["beta"]="mondal";
+    expect (data).toEqual({alpha:"koushik", beta:"mondal"});
 })
